a4f1d3d0554e9491e2b9297cc8a4496d4932a852
Kevin <reyeskevin707@gmail.com>
2020-08-20 22:12:55 -0700

    Update functions.js

--- api/utils/functions.js
+++ api/utils/functions.js
@@ -14,20 +14,21 @@ const handles ={
             client.message(message.text, {})
                 .then((data) => {
                     console.log('Yay, got Wit.ai response: ' + JSON.stringify(data));
-                    if(data.intents[0].name === 'introduction' && data.intents[0].confidence >= .50){
-                        if(data.traits.robotic[0].value === 'yes' && data.traits.robotic[0].confidence >= .50){
+                    if(data.intents[0].name & data.intents[0].name === 'introduction' && data.intents[0].confidence >= .50){
+                        if(data.traits.robotic[0].value & data.traits.robotic[0].value === 'yes' && data.traits.robotic[0].confidence >= .50){
                             res = {
                                 "text": `Beep Boop *robot noises*`
                             }
                             callSendAPI(sender_psid,res)
+                        } else{
+                            res = {
+                                "text": `Hello. :)`
+                            }
+                            callSendAPI(sender_psid,res)
                         }
-                        res = {
-                            "text": `Hello. :)`
-                        }
-                        callSendAPI(sender_psid,res)
                     } else{
                         res = {
-                            "text": `Yes`
+                            "text": `Hello there.`
                         }
                         callSendAPI(sender_psid,res)
                     }
